cadd63dd0647caab4a1160eefbf69fd5f080d5a0
wahyuWs <wahyusantozo36@gmail.com>
2023-10-25 11:49:21 +0700

    adding cardview

--- app/src/main/java/com/tutorial/rowcolumnsizing/MainActivity.kt
+++ app/src/main/java/com/tutorial/rowcolumnsizing/MainActivity.kt
@@ -31,6 +31,9 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -73,7 +76,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 
 }
 
